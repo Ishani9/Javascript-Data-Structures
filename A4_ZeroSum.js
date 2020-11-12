0430c4506d2f0bc8a8111ce470b34290b38ebd58
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-12 13:06:07 +0530

    Added: Program to show Sum of three Integer adds to ZERO

---
+++ A4_ZeroSum.js
@@ -0,0 +1,9 @@
+
+let numArray = new Array();
+numArray = [2, 3, -5];
+console.log("ARRAY IS : " + numArray);
+let sum = numArray[0];
+    for(let i = 1; i <= 3; i++){
+    console.log("ADDITION IS : " + sum);
+    sum = sum + numArray[i];
+    }
